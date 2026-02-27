777af26bca1ef9c6aede34331a00f9af9edf1247
musicode <musicode@qq.com>
2026-02-27 11:16:52 +0800

    新增金额的常用常量

--- src/constant/money.ts
+++ src/constant/money.ts
@@ -5,3 +5,9 @@ export const MONEY_TEN_THOUSAND_YUAN_TO_CENT = 10000 * MONEY_YUAN_TO_CENT
 
 // 元 转 厘
 export const MONEY_YUAN_TO_PENNY = 1000
+
+
+// 一元
+export const AMOUNT_ONE_YUAN = 100
+// 一万
+export const AMOUNT_TEN_THOUSAND_YUAN = 10000 * AMOUNT_ONE_YUAN
